8c0ece04142c2131d447d1cd17c7e2c1371e3cc9
abdulrahman ali <40067759+abodahim@users.noreply.github.com>
2025-08-21 01:47:06 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,9 +1,5 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-self.addEventListener('activate', (e) => {
-  return clients.claim();
-});
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => clients.claim());
 self.addEventListener('fetch', (e) => {
-  // تمرير الطلبات بدون كاش معقّد
+  // تمرير الطلبات كما هي — يمكن إضافة كاش لاحقًا
 });
