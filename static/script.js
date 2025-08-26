583b2383d8a141f9867694c85a2c82d207eda8dc
abdulrahman ali <40067759+abodahim@users.noreply.github.com>
2025-08-26 06:53:29 +0300

    Delete static/script.js

--- static/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // زر القائمة للجوال
-  const toggle = document.getElementById('menuToggle');
-  const nav = document.getElementById('mainNav');
-  if (toggle && nav) {
-    toggle.addEventListener('click', () => nav.classList.toggle('show'));
-  }
-});
